068d4aa63e21e82e8c10a9ca3e0607f52dad1bf1
Coder-DanielHan <2420626787@qq.com>
2018-05-10 08:20:00 +0800

    update

---
+++ Broadcast/app/src/main/java/java/github/danielhan/sample/broadcast/BootBroadReceiver.java
@@ -0,0 +1,14 @@
+package java.github.danielhan.sample.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootBroadReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Boot completed!", Toast.LENGTH_SHORT).show();
+    }
+}
