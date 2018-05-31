a84f0d36802e06c0cdd4ade07680f8261209c3a7
DanielHan <2420626787@qq.com>
2018-05-31 20:11:05 +0800

    动画

---
+++ TransitionSample/app/src/main/java/com/danielhan/transitionsample/MainActivity.java
@@ -0,0 +1,28 @@
+package com.danielhan.transitionsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+//                overridePendingTransition(R.anim.push_right_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+
+}
