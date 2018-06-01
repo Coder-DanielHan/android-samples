8675dec7d7bf07cbe3b961112cf8c6b2f48e1d2d
DanielHan <2420626787@qq.com>
2018-06-01 17:17:20 +0800

    动画

--- TransitionSample/app/src/main/java/com/danielhan/transitionsample/MainActivity.java
+++ TransitionSample/app/src/main/java/com/danielhan/transitionsample/MainActivity.java
@@ -2,6 +2,8 @@ package com.danielhan.transitionsample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +20,33 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
-//                overridePendingTransition(R.anim.push_right_in, R.anim.push_left_out);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                ActivityCompat.startActivity(MainActivity.this, intent,
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, btn, "btn").toBundle());
             }
         });
     }
 
+
+    public void explode(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("flag", "explode");
+        ActivityCompat.startActivity(MainActivity.this, intent,
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle());
+    }
+
+    public void slide(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("flag", "slide");
+        ActivityCompat.startActivity(MainActivity.this, intent,
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle());
+    }
+
+    public void fade(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("flag", "fade");
+        ActivityCompat.startActivity(MainActivity.this, intent,
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle());
+    }
 
 }
