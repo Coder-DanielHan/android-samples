4769c99a4253fc741042690dcd857c787229b8d9
Coder-DanielHan <2420626787@qq.com>
2018-06-18 10:34:12 +0800

    windowSoftInputMode的sample

--- WindowSoftInputMode/app/src/main/java/com/danielhan/windowsoftinputmode/MainActivity.java
+++ WindowSoftInputMode/app/src/main/java/com/danielhan/windowsoftinputmode/MainActivity.java
@@ -1,13 +1,30 @@
 package com.danielhan.windowsoftinputmode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+/**
+ * https://blog.csdn.net/zhaokaiqiang1992/article/details/39761461
+ */
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.edittext);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
     }
 }
